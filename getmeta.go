9e540b8f2b62b7fd4104a1e0f0e17ea72c6d30a2
Florian Obernbrger <jacquesanonym@gmail.com>
2021-11-08 10:16:15 +0100

    Update getmeta.go

--- getmeta.go
+++ getmeta.go
@@ -1,26 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/fatih/color"
 	"github.com/flobii/getmeta/stringsplus"
 	"github.com/gocolly/colly"
 )
 
-type metaelem struct {
+type metaElem struct {
 	Name    string
 	Content string
 }
 
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Usage: getmeta [url]\nCLI mode: getmeta --cli")
+		return
+	}
+	if os.Args[1] == "--cli" {
+		cliURL()
+		return
+	}
 	if len(os.Args) == 1 || len(os.Args) > 2 {
 		fmt.Println("Wrong number of arguments")
 		return
 	}
-	url := os.Args[1]
+	singleURL(os.Args[1])
+}
+
+func cliURL() {
+	const message string = "Enter URL to get meta. To exit press `Ctrl + D`"
+	fmt.Println(message)
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("URL: ")
+		url, err := reader.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				return
+			} else {
+				fmt.Println(err)
+			}
+		}
+		url = strings.Replace(url, "\n", "", -1)
+		if url != "" {
+			fmt.Println(strings.Repeat("─", utf8.RuneCountInString(url)+5))
+			singleURL(url)
+			fmt.Println()
+		} else {
+			fmt.Print()
+		}
+	}
+}
+
+func singleURL(url string) {
 	c := colly.NewCollector()
 	c.OnHTML("html", handleHTML)
 
@@ -46,13 +84,13 @@ func handleHTML(e *colly.HTMLElement) {
 	}
 }
 
-func formatMeta(names, contents []string) []metaelem {
+func formatMeta(names, contents []string) []metaElem {
 	max := stringsplus.GetMaxLength(names) + 1
-	meta := make([]metaelem, len(names))
+	meta := make([]metaElem, len(names))
 	for i, name := range names {
 		if len(name) < max {
 			name = name + ":" + strings.Repeat(" ", max-len(name))
-			meta[i] = metaelem{name, contents[i]}
+			meta[i] = metaElem{name, contents[i]}
 		}
 	}
 	return meta
